731cff5ac82ea4ccb170ed9fdf3cda4b992ebc15
Greg Albers <galbers@getty.edu>
2015-06-05 21:19:09 -0700

    path adjustment

--- pe-epub_runscript-animate.js
+++ pe-epub_runscript-animate.js
@@ -1,8 +1,8 @@
 var Peepub   = require('pe-epub-fs')(require('pe-epub'));
-var epubJson = require('/Users/gregalbers/GitHub/EBOOKS/animate-epub/animate.json'); // see examples/example.json for the specs
+var epubJson = require('/Users/galbers/GitHub/animate-epub/animate.json'); // see examples/example.json for the specs
 var myPeepub = new Peepub(epubJson);
 
-myPeepub.create('/Users/gregalbers/animate.epub')
+myPeepub.create('/Users/galbers/GitHub/animate-epub/animate.epub')
     .then(function(filePath){
         console.log(filePath); // the same path to your epub file!
     });
